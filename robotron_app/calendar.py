d8f7922185fdaae458bb6a8fda6040a9cab44db8
Thiev <a.thiev@gmail.com>
2018-10-09 00:26:56 +0200

    calendar init work

--- robotron_app/calendar.py
+++ robotron_app/calendar.py
@@ -7,17 +7,17 @@ from django.shortcuts import render
 
 
 def generate_month_manual(year_number=(datetime.now().year), month_number=(datetime.now().month)):
-    # start_day = datetime(year_number, month_number, 1).weekday()
+
     start_day = monthrange(year_number,month_number)[0]
     month_length = monthrange(year_number,month_number)[1]
     month_names = {
         1:'January', 2:'February', 3:'March', 4:'April', 5:'May', 6:'June',
         7:'July', 8:'August', 9:'September', 10:'October', 11:'November', 12:'December'
     }
-    day_titles = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+    day_titles = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
 
     month_text = ''
-    month_text += '<table class="table table-hover" id="calendar_month"><thead class="thead-light">'+'\n'
+    month_text += '<table class="table table-hover table-responsive" id="calendar_month"><thead class="thead-light">'+'\n'
     month_text += '<tr class="text-center align-middle">'+'\n'
     month_text += '<th colspan="1"><a role="button" id="nav_prev" class="btn btn-sm btn-outline-secondary" href="#"> << </a></th>'+'\n'
     month_text += '<th colspan="5" class="month_title">'+month_names[month_number]+' '+str(year_number)+'</th>'+'\n'
@@ -26,23 +26,24 @@ def generate_month_manual(year_number=(datetime.now().year), month_number=(datet
     month_text += '<tr>'+'\n'
 
     for day_title in day_titles:
-        month_text+='<th class="day_title">'+day_title+'</th>'+'\n'
+        month_text+='<th scope="col" class="day_title">'+day_title+'</th>'+'\n'
 
     month_text += '</tr></thead>'+'\n'
     month_text += '<tbody>'+'\n'
     month_text += '<tr class="row_clickable">'+'\n'
 
     # pre-month empty cells
-    for i in range(0, start_day+1):
+    for i in range(0, start_day):
         month_text += '<td class="day"></td>' + '\n'
 
     day = 1
 
-    for i in range(start_day+1,7):
+    for i in range(start_day, 7):
         # if (day in month_events.keys()):
         #     month_text += insert_event(day, month_events[day][0], month_events[day][1], month_events[day][2])
         # else:
-        month_text += '<td class="day">' + str(day) + '</td>' + '\n'
+        day_id = str(year_number)+'-'+str(month_number)+'-'+str(day)
+        month_text += '<td class="day day-clickable" id="'+day_id+'">' + str(day) + '</td>' + '\n'
         day += 1
 
     month_text += '</tr>' + '\n'
@@ -54,7 +55,8 @@ def generate_month_manual(year_number=(datetime.now().year), month_number=(datet
 
         for i in range(0, 7):
             if (day <= month_length):
-                month_text += '<td class="day">' + str(day) + '</td>' + '\n'
+                day_id = str(year_number) + '-' + str(month_number) + '-' + str(day)
+                month_text += '<td class="day day-clickable" id="' + day_id + '">' + str(day) + '</td>' + '\n'
                 day += 1
             else:
                 month_text += '<td class="day"></td>' + '\n'
