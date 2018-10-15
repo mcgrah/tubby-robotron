dddff53fb8287b61520873125b13568df714f8bc
Thiev <a.thiev@gmail.com>
2018-10-15 16:00:09 +0200

    clean userlist

--- robotron_app/templatetags/filters.py
+++ robotron_app/templatetags/filters.py
@@ -1,5 +1,6 @@
 import datetime
 from django import template
+from robotron_app.models import Studio
 register = template.Library()
 
 @register.filter
@@ -17,3 +18,14 @@ def isRoboto(user):
     if user.is_superuser:
         return True
     return user.groups.filter(name='Roboto Users').exists()
+
+@register.filter
+def hasStudio(user):
+    try:
+        studio = Studio.objects.get(user=user)
+        print(f'found: {studio}')
+        return studio
+    except Exception as e:
+        print(e)
+        pass
+    return None
